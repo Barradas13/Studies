7743f31fa60bff1fe63d518798cbf93d82a22de5
Felipe Barradas Sebastião <febarradas13@gmail.com>
2023-09-24 11:47:06 -0300

    Conjuntos functions

---
+++ conjuntos.py
@@ -0,0 +1,7 @@
+c1 = {1,2,3,4,5,6,7}
+c2 = {6,7,8,9,10}
+
+print("União = | :", c1 | c2 )
+print("Diferença c1 p c2 = - :", c1 - c2 )
+print("Diferença c2 p c1 = - :", c2 - c1 )
+print("Intersecção = & :", c1 & c2 )
