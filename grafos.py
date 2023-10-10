acac6231b03c06406a3e3792d7da4723d3216984
Felipe Barradas Sebastião <febarradas13@gmail.com>
2023-10-10 11:37:00 -0300

    mudando o método de leitura por largura

--- grafos.py
+++ grafos.py
@@ -1,9 +1,9 @@
 grafo = {
-    1:[2],
+    1:[2,3],
     2:[5],
-    3:[1],
-    4:[3],
-    5:[],
+    3:[4],
+    4:[1],
+    5:[1],
 }
 
 def profundidade(destino, origem, grafo = grafo):
@@ -19,20 +19,51 @@ def profundidade(destino, origem, grafo = grafo):
                     achou = True
     return achou
 
-def largura(destino, origem, grafo = grafo):
-    achou = False
-    if destino in origem:
-        achou = True
-    else:
-        nova_origem = set()
-        for i in origem:
-            for j in grafo[i]:
-                nova_origem.add(j) 
-        if nova_origem != origem:
-            if largura(destino, nova_origem, grafo):
-                achou = True
 
-    return achou
+import heapq
 
-print(profundidade(1, [5]))
-print(largura(1, {5}))
+def largura(grafo, r):
+    #cores = lista para definir o status do vértice, branco - ainda não passou; cinza - passando; preto - ja passou;
+    cores = dict()
+    #pais - lista para definir de onde vem para chegar;
+    pais = dict()
+    #distancia para chegar do r para o vertice desejado;
+    distancias = dict()
+
+    #lista vertices serve para definir quais nós estão sendo passados no momento;
+    vertices = [r]
+    heapq.heapify(vertices)
+
+    #definindo as listas para retorno todas por enquanto sem distancia e pais e brancas (não foram passadas ainda)
+    for i in grafo:
+        cores[i] = "branco"
+        pais[i] = None
+        distancias[i] = None
+    
+    #se não tiver mais vértices para olhar acaba
+    while len(vertices) > 0:
+        #new vertices serve para definir quais vão ser os novos nós a se passar
+        new_vertices = []
+        heapq.heapify(new_vertices)
+        distancias[r] = 0
+        pais[r] = r
+        
+        #para cada aresta do vestice atual é visto qual é o lugar que chega, sua cor e dependendo desta se define resultados
+        for i in range(len(vertices)):
+            cores[vertices[0]] = "cinza"
+            for j in grafo[vertices[0]]:
+                if cores[j] == "branco":
+                    pais[j] = vertices[0]
+                    distancias[j] = distancias[vertices[0]] + 1
+                    
+                    heapq.heappush(new_vertices, j)
+                    
+            cores[vertices[0]] = "preto"
+            heapq.heappop(vertices)
+
+        vertices = new_vertices.copy()
+    
+    print(distancias, cores, pais)
+
+
+largura(grafo, 1)
